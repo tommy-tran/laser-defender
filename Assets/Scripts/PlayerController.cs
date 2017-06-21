862fec6490b11fab787b03d64f3404fa486e7fde
tommy-tran <tome7@live.com>
2017-06-21 12:38:05 -0400

    Restricting horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public Object ship;
 	public float speed = 10;
+	public float padding = 0.1f;
+
+	float xmin;
+	float xmax;
 
 	// Use this for initialization
 	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xmax = rightMost.x - padding;
+		xmin = leftMost.x + padding;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
+
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, xmin, xmax), transform.position.y, transform.position.z);
 	}
 }
