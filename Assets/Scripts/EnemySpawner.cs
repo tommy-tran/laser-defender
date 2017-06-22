9132aa792bd6ee11a1368aeebf530c690f970c21
tommy-tran <tome7@live.com>
2017-06-22 19:12:33 -0400

    Created enemy spawner and first enemy prefab

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate (enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject; // needs as GameObject because Instantiate creates Objects
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
