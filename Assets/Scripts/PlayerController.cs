5baef2e643bc611c42b103b304ae1dc83a7ecb4e
tommy-tran <tome7@live.com>
2017-06-25 06:05:55 -0400

    Added projectile spawn and garbage collection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed = 10;
 	public float padding = 0.1f;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public float fireRate = 0.2f;
 
 	float xmin;
 	float xmax;
@@ -19,11 +22,28 @@ public class PlayerController : MonoBehaviour {
 		xmin = leftMost.x + padding;
 	}
 	
+    void Fire()
+    {
+        GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+    }
+
+
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.0000001f, fireRate);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
+
+
+
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
-
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
