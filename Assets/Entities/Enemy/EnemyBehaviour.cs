e5da31369899e4c118bd5e8db25716779d5bc5c5
tommy-tran <tome7@live.com>
2017-06-25 06:38:29 -0400

    Added damage against enemies

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    public float health = 200f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile missile = collision.gameObject.GetComponent<Projectile>();
+        if (missile)
+        {
+            Debug.Log(missile.getDamage());
+            health -= missile.getDamage();
+            missile.Hit();
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
